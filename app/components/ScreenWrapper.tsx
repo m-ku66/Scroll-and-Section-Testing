02f6e1264aa5b44eada8e5b67e8c30ac1854e6ab
m-ku66 <johanmarcmiango15@gmail.com>
2025-03-28 13:30:09 -0400

    scroll bar edits again. Trying to make it look more like a meter...

--- app/components/ScreenWrapper.tsx
+++ app/components/ScreenWrapper.tsx
@@ -33,15 +33,29 @@ const ScreenWrapper = ({ children }: ScreenWrapperProps) => {
       })}
 
       {/* Scroll progress indicator */}
-      <div className="fixed top-0 right-10 w-1 h-full bg-transparent z-50">
-        <motion.div
-          className="w-full bg-neutral-800 origin-top"
-          style={{
-            scaleY: scrollYProgress,
-            height: "100%",
-          }}
-        />
-      </div>
+      <motion.div
+        className="fixed top-1/2 right-10 h-1/2 w-1 bg-transparent z-50 -translate-y-1/2"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+      >
+        <div className="w-full h-full bg-gray-200/30 rounded-full overflow-hidden relative">
+          {/* Dots along the track */}
+          <div className="absolute top-0 left-1/2 w-1 h-1 bg-gray-400 rounded-full -translate-x-1/2"></div>
+          <div className="absolute top-1/4 left-1/2 w-1 h-1 bg-gray-400 rounded-full -translate-x-1/2"></div>
+          <div className="absolute top-1/2 left-1/2 w-1 h-1 bg-gray-400 rounded-full -translate-x-1/2"></div>
+          <div className="absolute top-3/4 left-1/2 w-1 h-1 bg-gray-400 rounded-full -translate-x-1/2"></div>
+          <div className="absolute bottom-0 left-1/2 w-1 h-1 bg-gray-400 rounded-full -translate-x-1/2"></div>
+
+          <motion.div
+            className="w-full bg-neutral-800 origin-top rounded-full"
+            style={{
+              scaleY: scrollYProgress,
+              height: "100%",
+            }}
+          />
+        </div>
+      </motion.div>
     </div>
   );
 };
